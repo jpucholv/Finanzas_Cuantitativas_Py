b0f68f6a4f1143972d27e64e2e6b8be834a34bf0
jpucholv <96960258+jpucholv@users.noreply.github.com>
2023-09-24 17:49:15 +0200

    06 concluir primera clase en Python y cómo enviar tareas por Google Drive

--- 2024_1/01_rv_sim.py
+++ 2024_1/01_rv_sim.py
@@ -17,104 +17,11 @@ importlib.reload(random_variables)
 
 # inputs
 coeff = 5 # df in student, scale in exponential
-random_variable_type = 'student' # options: normal student uniform exponential chi-squared
+random_variable_type = 'normal' # options: normal student uniform exponential chi-squared
 size = 10**6
 decimals = 5
 
 sim = random_variables.simulator(coeff, random_variable_type, size, decimals)
 sim.generate_vector()
-x = sim.vector
-
-# task 1
-# compute the RV vector
-# DONE: sim.generate_vector()
-
-# task 2
-# compute stats on the RV vector
-# including the Jarque-Bera test
-
-# task 3
-# plot
-
-# code
-str_title = random_variable_type
-if random_variable_type == 'normal':
-    x = np.random.standard_normal(size=size)
-
-elif random_variable_type == 'student':
-    x = np.random.standard_t(df=coeff, size=size)
-    str_title += ' df=' + str(coeff)
-
-elif random_variable_type == 'uniform':
-    x = np.random.uniform(size=size)
-
-elif random_variable_type == 'exponential':
-    x = np.random.exponential(scale=coeff, size=size)
-    str_title += ' scale=' + str(coeff)
-    
-elif random_variable_type == 'chi-squared':
-    x = np.random.chisquare(df=coeff, size=size)
-    str_title += ' df=' + str(coeff)
-    
-mu = st.tmean(x) # np.mean st.tmean
-sigma = st.tstd(x) # np.std st.tstd
-skewness = st.skew(x)
-kurtosis = st.kurtosis(x)
-
-# Test of Normality: Jarque-Bera
-jb_stat = size/6 * (skewness**2 + 1/4 * kurtosis**2)
-p_value = 1 - st.chi2.cdf(jb_stat, df=2)
-is_normal = (p_value > 0.05) # equivalently jb < 6
-
-str_title += '\n' + 'mean=' + str(np.round(mu, decimals)) \
-    + ' | ' + 'volatility=' + str(np.round(sigma, decimals)) \
-    + '\n' + 'skewness=' + str(np.round(skewness, decimals)) \
-    + ' | ' + 'kurtosis=' + str(np.round(kurtosis, decimals)) \
-    + '\n' + 'JB stat=' + str(np.round(jb_stat, decimals)) \
-    + ' | ' + 'p-value' + str(np.round(p_value, decimals)) \
-    + ' | ' + 'is_normal=' + str(is_normal)
-    
-# plot
-plt.figure()
-plt.hist(x, bins=100)
-plt.title(str_title)
-plt.show()
-
-
-####################################
-# Loop of Jarque-Bera normality test
-####################################
-
-n = 0
-is_normal = True
-str_title = 'normal'
-
-while is_normal and n < 100:
-    x = np.random.standard_normal(size=size)
-    
-    mu = st.tmean(x) # np.mean st.tmean
-    sigma = st.tstd(x) # np.std st.tstd
-    skewness = st.skew(x)
-    kurtosis = st.kurtosis(x)
-
-    # Test of Normality: Jarque-Bera
-    jb_stat = size/6 * (skewness**2 + 1/4 * kurtosis**2)
-    p_value = 1 - st.chi2.cdf(jb_stat, df=2)
-    is_normal = (p_value > 0.05) # equivalently jb < 6
-    
-    print(f'n={n} | is_normal={is_normal}')
-    n += 1
-
-str_title += '\n' + 'mean=' + str(np.round(mu, decimals)) \
-    + ' | ' + 'volatility=' + str(np.round(sigma, decimals)) \
-    + '\n' + 'skewness=' + str(np.round(skewness, decimals)) \
-    + ' | ' + 'kurtosis=' + str(np.round(kurtosis, decimals)) \
-    + '\n' + 'JB stat=' + str(np.round(jb_stat, decimals)) \
-    + ' | ' + 'p-value' + str(np.round(p_value, decimals)) \
-    + ' | ' + 'is_normal=' + str(is_normal)
-    
-# plot
-plt.figure()
-plt.hist(x, bins=100)
-plt.title(str_title)
-plt.show()
+sim.compute_stats()
+sim.plot()
--- 2024_1/random_variables.py
+++ 2024_1/random_variables.py
@@ -20,24 +20,51 @@ class simulator():
         self.decimals = decimals
         self.str_title = None
         self.vector = None
+        self.mean = None
+        self.volatility = None
+        self.skewness = None
+        self.kurtosis = None
+        self.jb_stat = None
+        self.p_value = None
+        self.is_normal = None
         
     def generate_vector(self):
         self.str_title = self.rv_type
         
         if self.rv_type == 'normal':
             self.vector = np.random.standard_normal(size=self.size)
-
         elif self.rv_type == 'student':
             self.vector = np.random.standard_t(df=self.coeff, size=self.size)
             self.str_title += ' df=' + str(self.coeff)
-
         elif self.rv_type == 'uniform':
             self.vector = np.random.uniform(size=self.size)
-
         elif self.rv_type == 'exponential':
             self.vector = np.random.exponential(scale=self.coeff, size=self.size)
-            self.str_title += ' scale=' + str(self.coeff)
-            
+            self.str_title += ' scale=' + str(self.coeff)            
         elif self.rv_type == 'chi-squared':
             self.vector = np.random.chisquare(df=self.coeff, size=self.size)
             self.str_title += ' df=' + str(self.coeff)
+            
+    def compute_stats(self):
+        self.mean = st.tmean(self.vector)
+        self.volatility = st.tstd(self.vector)
+        self.skewness = st.skew(self.vector)
+        self.kurtosis = st.kurtosis(self.vector)
+        self.jb_stat = self.size/6 * (self.skewness**2 + 1/4*self.kurtosis**2)
+        self.p_value = 1 - st.chi2.cdf(self.jb_stat, df=2)
+        self.is_normal = (self.p_value > 0.05) # equivalently jb < 6
+        
+    def plot(self, bins=100):
+        self.str_title += '\n' + 'mean=' + str(np.round(self.mean, self.decimals)) \
+            + ' | ' + 'volatility=' + str(np.round(self.volatility, self.decimals)) \
+            + '\n' + 'skewness=' + str(np.round(self.skewness, self.decimals)) \
+            + ' | ' + 'kurtosis=' + str(np.round(self.kurtosis, self.decimals)) \
+            + '\n' + 'JB stat=' + str(np.round(self.jb_stat, self.decimals)) \
+            + ' | ' + 'p-value' + str(np.round(self.p_value, self.decimals)) \
+            + ' | ' + 'is_normal=' + str(self.is_normal)
+            
+        # plot
+        plt.figure()
+        plt.hist(self.vector, bins)
+        plt.title(self.str_title)
+        plt.show()
